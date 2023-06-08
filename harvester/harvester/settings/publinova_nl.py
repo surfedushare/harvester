30ab4ac24a3a2949af151f51f057ff3340363d81
Fako Berkers <email@fakoberkers.nl>
2023-06-08 11:22:23 +0200

    Switches development environment to DEBUG=False for Publinova

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -8,3 +8,8 @@ ALLOW_CLOSED_ACCESS_DOCUMENTS = True
 LOWEST_EDUCATIONAL_LEVEL = -1  # will ignore lowest educational level requirements
 
 SHAREKIT_TEST_ORGANIZATION = "Publinova test"
+
+# While Zooma develops against our development environment we switch to DEBUG=False
+# To do this we need to loosen IP restrictions. We want to prevent leaking debug information to the world.
+if environment.service.env == "development":
+    DEBUG = False
