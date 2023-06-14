2d7ab32c69ab78fc6a479c8278023bc0c9f1e8a7
Fako Berkers <email@fakoberkers.nl>
2023-06-14 14:45:48 +0200

    Fixes typo

--- harvester/core/migrations/0047_publinova_products.py
+++ harvester/core/migrations/0047_publinova_products.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0046_edurep_jsonresearch'),
+        ('core', '0046_edurep_jsonsearch'),
     ]
 
     operations = [
