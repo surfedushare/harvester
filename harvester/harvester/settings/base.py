b35df660ede3b2553e3fc2690ffd23c7aaa68d6d
Fako Berkers <email@fakoberkers.nl>
2023-06-26 18:58:24 +0200

    Places environments.utils under system_configuration

--- harvester/harvester/settings/base.py
+++ harvester/harvester/settings/base.py
@@ -21,7 +21,7 @@ from sentry_sdk.integrations.logging import ignore_logger
 from celery.schedules import crontab
 
 from system_configuration.main import create_configuration_and_session, MODE, PROJECT
-from utils.packaging import get_package_info
+from system_configuration.packaging import get_package_info
 from search_client.version import VERSION as SEARCH_CLIENT_VERSION
 from search_client.opensearch.logging import OpensearchHandler, create_opensearch_handler
 
