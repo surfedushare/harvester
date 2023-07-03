8d8b628919dc7d0a198cf79312c99a147df37470
Kalle Pronk <kallepronk@gmail.com>
2023-07-03 14:27:11 +0200

    Added next parameter to edurep json resource

--- harvester/sources/models/edurep.py
+++ harvester/sources/models/edurep.py
@@ -50,3 +50,12 @@ class EdurepJsonSearchResource(HarvestHttpResource):
                    "%20AND%20%28schema%3AeducationalLevel.schema%3AtermCode%20exact%20" \
                    "bbbd99c6-cf49-4980-baed-12388f8dcff4%20OR%20schema%3AeducationalLevel.schema%3A" \
                    "termCode%20exact%20be140797-803f-4b9e-81cc-5572c711e09c%29"
+
+    def next_parameters(self):
+        content_type, data = self.content
+        page = data["response"].get("next", None).get("page", None)
+        if not page:
+            return {}
+        return {
+            "page": page,
+        }
