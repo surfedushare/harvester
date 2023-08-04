2881b31111e3db2dbf502c341316e4fba2e34273
Fako Berkers <email@fakoberkers.nl>
2023-08-04 10:56:26 +0200

    Removes deprecated Resource models from core

--- harvester/core/models/__init__.py
+++ harvester/core/models/__init__.py
@@ -1,21 +1,17 @@
-from .ims import CommonCartridge
-
 from .resources.harvest import HarvestHttpResource
 from .resources.basic import HttpTikaResource, ExtructResource
-from .resources.youtube_dl import YouTubeDLResource
 from .resources.matomo import MatomoVisitsResource
-from .resources.thumbnails import YoutubeThumbnailResource, PdfThumbnailResource  # legacy models
 
+# Start legacy models
+from .resources.thumbnails import YoutubeThumbnailResource, PdfThumbnailResource
 from .legacy.dataset import Dataset, DatasetVersion
 from .legacy.collection import Collection
 from .legacy.document import Document
 from .legacy.pipeline import Batch, ProcessResult
 from .legacy.extension import Extension
-
 from .legacy.harvest import Harvest, HarvestSource
+# End legacy models
 
 from .search import ElasticIndex, ElasticIndexSerializer, Query
-
-from .extraction import ExtractionMapping, ExtractionMethod, MethodExtractionField, JSONExtractionField
 
 from .choices import EducationalLevels
