327434aa14b228b6f06545c9a36b8c65cbf0928a
Fako Berkers <email@fakoberkers.nl>
2023-08-21 10:59:53 +0200

    Prevents aggregation of frequencies for particular MetadataFields

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -8,3 +8,5 @@ ALLOW_CLOSED_ACCESS_DOCUMENTS = True
 LOWEST_EDUCATIONAL_LEVEL = -1  # will ignore lowest educational level requirements
 
 SHAREKIT_TEST_ORGANIZATION = "Publinova test"
+
+SIMPLE_METADATA_FREQUENCY_FIELDS = []
