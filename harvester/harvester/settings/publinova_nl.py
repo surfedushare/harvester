3b7198c99971960bc1fd44f57a71c7679352622a
Fako Berkers <email@fakoberkers.nl>
2023-09-05 17:36:20 +0200

    Hard override for Sharekit base url on acceptance for Publinova

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -10,3 +10,8 @@ LOWEST_EDUCATIONAL_LEVEL = -1  # will ignore lowest educational level requiremen
 SHAREKIT_TEST_ORGANIZATION = "Publinova test"
 
 SIMPLE_METADATA_FREQUENCY_FIELDS = []
+
+# This is a temporary hard coded override that's hard to achieve with environment variables alone.
+# Can be removed if Edusources and Publinova agree on what source to use for Sharekit on acceptance.
+if MODE == "acceptance":
+    SHAREKIT_BASE_URL = "https://api.acc.surfsharekit.nl"
