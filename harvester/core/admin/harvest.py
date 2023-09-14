8341ab4b021319861eaabe21d5a976099ed23abe
Fako Berkers <email@fakoberkers.nl>
2023-09-14 11:09:41 +0200

    Adds harvesting indicators to HarvestState admin

--- harvester/core/admin/harvest.py
+++ harvester/core/admin/harvest.py
@@ -15,5 +15,9 @@ class HarvestAdminInline(admin.TabularInline):
 
 
 class HarvestStateAdmin(admin.ModelAdmin):
-    list_display = ("entity", "dataset", "set_specification", "harvested_at",)
+    list_display = ("entity", "dataset", "set_specification", "harvested_at", "is_harvesting")
     list_filter = ("entity", "dataset",)
+
+    def is_harvesting(self, obj):
+        return bool(obj.harvest_set and obj.harvest_set.pending_at or obj.harvest_set.dataset_version.pending_at)
+    is_harvesting.boolean = True
