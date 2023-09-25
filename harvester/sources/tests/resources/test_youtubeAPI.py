fce729cea1af89e0aac3d15bd4f19f034ace24c0
Kalle Pronk <kallepronk@gmail.com>
2023-09-25 14:19:51 +0200

    add youtubeAPIResource migration

---
+++ harvester/sources/tests/resources/test_youtubeAPI.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from unittest.mock import patch
+
+from files.models import YoutubeAPIResource
+
+
+class TestYoutubeAPIResource(TestCase):
+
+    @patch("files.models.resources.youtube_api.YoutubeAPIResource._send")
+    def test_regex(self, send_mock):
+        urls = [
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "http://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=feedrec_grec_index",
+            "http://www.youtube.com/v/dQw4w9WgXcQ?fs=1&amp;hl=en_US&amp;rel=0",
+            "http://www.youtube.com/watch?v=dQw4w9WgXcQ#t=0m10s",
+            "http://www.youtube.com/embed/dQw4w9WgXcQ?rel=0",
+            "http://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "http://youtu.be/dQw4w9WgXcQ"
+
+        ]
+
+        for url in urls:
+            resource = YoutubeAPIResource().get(url)
+            self.assertEqual(resource.request_without_auth()["url"],
+                             "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cplayer&id=dQw4w9WgXcQ")
