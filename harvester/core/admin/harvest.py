9d671646a4a8be34fc7937fd94839d13422ca93e
Fako Berkers <email@fakoberkers.nl>
2023-10-04 14:00:45 +0200

    Fixes HarvestState is_harvesting indicator

--- harvester/core/admin/harvest.py
+++ harvester/core/admin/harvest.py
@@ -19,5 +19,5 @@ class HarvestStateAdmin(admin.ModelAdmin):
     list_filter = ("entity", "dataset",)
 
     def is_harvesting(self, obj):
-        return bool(obj.harvest_set and obj.harvest_set.pending_at or obj.harvest_set.dataset_version.pending_at)
+        return bool(obj.harvest_set and (obj.harvest_set.pending_at or obj.harvest_set.dataset_version.pending_at))
     is_harvesting.boolean = True
