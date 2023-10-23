35ab28f43ac76c8e3d9cd4c74c166765a00e890e
Kalle Pronk <kallepronk@gmail.com>
2023-10-23 11:29:19 +0200

    fix wrong class import in sources admin

--- harvester/sources/models/__init__.py
+++ harvester/sources/models/__init__.py
@@ -5,7 +5,7 @@ from .greeni import GreeniOAIPMHResource
 from .buas import BuasPureResource
 from .hanze import HanzeResearchObjectResource
 from .edurep_json import EdurepJsonSearchResource
-from .edurep import EdurepOAIPMHBase
+from .edurep import EdurepOAIPMH
 from .publinova import PublinovaMetadataResource
 from .saxion import SaxionOAIPMHResource
 from .harvest import HarvestSource, HarvestEntity
