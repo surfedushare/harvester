5e0f69d08b9267d098964add0b6cd36b61e1aa9d
Kalle Pronk <kallepronk@gmail.com>
2023-11-17 11:20:53 +0100

    add edurep file tests

--- harvester/files/tests/seeding/test_edurep.py
+++ harvester/files/tests/seeding/test_edurep.py
@@ -16,6 +16,7 @@ class TestEdurepFileSeeding(TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.set = FileSet.objects.create(name="edurep", identifier="srn")
+        import ipdb; ipdb.set_trace()
         self.processor = HttpSeedingProcessor(self.set, {
             "phases": SEEDING_PHASES
         })
@@ -31,25 +32,26 @@ class TestEdurepFileSeeding(TestCase):
         self.assertEqual(self.set.documents.count(), 12)
 
 
-# class TestEdurepFileExtraction(TestCase):
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#         EdurepOAIPMHFactory.create_common_responses()
-#
-#     def setUp(self) -> None:
-#         super().setUp()
-#         self.set = FileSet.objects.create(name="edusources", identifier="srn")
-#         self.processor = HttpSeedingProcessor(self.set, {
-#             "phases": SEEDING_PHASES
-#         })
-#         self.seeds = []
-#         for batch in self.processor("edusources", "1970-01-01T00:00:00Z"):
-#             self.seeds += [doc.properties for doc in batch]
-#
-#     def test_get_hash(self):
-#         self.assertEqual(self.seeds[0]["hash"], "0ed38cdc914e5e8a6aa1248438a1e2032a14b0de")
-#
-#     def test_get_external_id(self):
-#         self.assertEqual(self.seeds[0]["external_id"], "0ed38cdc914e5e8a6aa1248438a1e2032a14b0de")
+class TestEdurepFileExtraction(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        EdurepOAIPMHFactory.create_common_responses()
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.set = FileSet.objects.create(name="edurep", identifier="srn")
+        self.processor = HttpSeedingProcessor(self.set, {
+            "phases": SEEDING_PHASES
+        })
+        self.seeds = []
+        for batch in self.processor("edurep", "1970-01-01T00:00:00Z"):
+            self.seeds += [doc.properties for doc in batch]
+
+    def test_get_hash(self):
+        import ipdb; ipdb.set_trace()
+        self.assertEqual(self.seeds[0]["hash"], "0ed38cdc914e5e8a6aa1248438a1e2032a14b0de")
+
+    def test_get_external_id(self):
+        self.assertEqual(self.seeds[0]["external_id"], "0ed38cdc914e5e8a6aa1248438a1e2032a14b0de")
