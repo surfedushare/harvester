7f8993c9fc4c4b109c1f7cf9cc1ea5b965d07c38
Fako Berkers <email@fakoberkers.nl>
2023-11-21 14:41:56 +0100

    Renames core to legacy in compatability tests

--- harvester/testing/tests/backward_compatability/test_document.py
+++ harvester/testing/tests/backward_compatability/test_document.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from core.models import Document as CoreDocument
+from core.models import Document as LegacyDocument
 from products.models import ProductDocument
 
 
@@ -28,46 +28,46 @@ class TestProductDocumentCompatability(TestCase):
 
     def setUp(self):
         super().setUp()
-        self.core_document = CoreDocument.objects.get(id=1)
+        self.legacy_document = LegacyDocument.objects.get(id=1)
         self.product_document = ProductDocument.objects.get(id=1)
 
     def test_to_search(self):
-        core_search = list(self.core_document.to_search())[0]  # legacy Document doesn't provide an easier way
+        legacy_search = list(self.legacy_document.to_search())[0]  # legacy Document doesn't provide an easier way
         product_search = self.product_document.to_data(for_search=True)
-        # First we check all the fields that should remain equal between core's Document and ProductDocument
+        # First we check all the fields that should remain equal between LegacyDocument and ProductDocument
         for field in self.backward_compatible_fields:
-            self.assertIn(field, core_search, f"Core Document is missing search field: {field}")
+            self.assertIn(field, legacy_search, f"LegacyDocument is missing search field: {field}")
             self.assertIn(field, product_search, f"ProductDocument is missing search field: {field}")
-            self.assertEqual(core_search[field], product_search[field],
-                             f"Search field '{field}' differs for core and product documents")
-        self.assertEqual(len(core_search["files"]), len(product_search["files"]),
-                         "Core Document and ProductDocument should have the same number of files")
-        # Then we check if file objects on ProductDocument have at least core's file object keys and values
+            self.assertEqual(legacy_search[field], product_search[field],
+                             f"Search field '{field}' differs for legacy and product documents")
+        self.assertEqual(len(legacy_search["files"]), len(product_search["files"]),
+                         "LegacyDocument and ProductDocument should have the same number of files")
+        # Then we check if file objects on ProductDocument have at least legacy file object keys and values
         # ProductDocument will expose more data in its file object, but that's irrelevant for compatibility
-        for core_file, product_file in zip(core_search["files"], product_search["files"]):
-            for file_field in core_file.keys():
-                self.assertEqual(core_file[file_field], product_file[file_field],
-                                 f"File field '{file_field}' differs for core and product files")
-        # Then we check if deprecated fields in core's Document no longer exist in ProductDocument
+        for legacy_file, product_file in zip(legacy_search["files"], product_search["files"]):
+            for file_field in legacy_file.keys():
+                self.assertEqual(legacy_file[file_field], product_file[file_field],
+                                 f"File field '{file_field}' differs for legacy and product files")
+        # Then we check if deprecated fields in LegacyDocument no longer exist in ProductDocument
         for field in self.removed_fields:
             self.assertNotIn(field, product_search)
 
     def test_to_search_no_files(self):
-        # We're removing files data and fields of core Document that only get set when files are known during harvest
-        self.core_document.properties["files"] = []
+        # We're removing files data and fields of LegacyDocument that only get set when files are known during harvest
+        self.legacy_document.properties["files"] = []
         self.product_document.properties["files"] = []
         for field in self.file_dependant_fields:
-            self.core_document.properties[field] = None
+            self.legacy_document.properties[field] = None
 
-        core_search = list(self.core_document.to_search())[0]  # legacy Document doesn't provide an easier way
+        legacy_search = list(self.legacy_document.to_search())[0]  # legacy Document doesn't provide an easier way
         product_search = self.product_document.to_data(for_search=True)
 
-        # We make sure that any "file" fields on core Document have the same default as ProductDocument
+        # We make sure that any "file" fields on LegacyDocument have the same default as ProductDocument
         for field in self.backward_compatible_fields:
-            self.assertIn(field, core_search, f"Core Document is missing search field: {field}")
+            self.assertIn(field, legacy_search, f"LegacyDocument is missing search field: {field}")
             self.assertIn(field, product_search, f"ProductDocument is missing search field: {field}")
-            self.assertEqual(core_search[field], product_search[field],
-                             f"Search field '{field}' differs for core and product documents")
+            self.assertEqual(legacy_search[field], product_search[field],
+                             f"Search field '{field}' differs for legacy and product documents")
         # No files should exist in the output
-        self.assertEqual(len(core_search["files"]), 0)
+        self.assertEqual(len(legacy_search["files"]), 0)
         self.assertEqual(len(product_search["files"]), 0)
