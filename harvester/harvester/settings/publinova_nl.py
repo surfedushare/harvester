406358cbb8e0cbfc43887f2b20d289972ba90ce2
Fako Berkers <email@fakoberkers.nl>
2023-11-22 09:41:21 +0100

    Removes Sharekit API overwrite for Publinova acceptance

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -7,8 +7,3 @@ DOCUMENT_TYPE = DocumentTypes.RESEARCH_PRODUCT
 SHAREKIT_TEST_ORGANIZATION = "Publinova test"
 
 SIMPLE_METADATA_FREQUENCY_FIELDS = []
-
-# This is a temporary hard coded override that's hard to achieve with environment variables alone.
-# Can be removed if Edusources and Publinova agree on what source to use for Sharekit on acceptance.
-if MODE == "acceptance":
-    SHAREKIT_BASE_URL = "https://api.acc.surfsharekit.nl"
