fc37155649ce7516b0a0608ac15d06e64666a9a1
Kalle Pronk <kallepronk@gmail.com>
2023-11-27 14:03:47 +0100

    finish edurep file extraction

--- harvester/files/sources/edurep.py
+++ harvester/files/sources/edurep.py
@@ -3,51 +3,15 @@ from typing import Iterator
 from hashlib import sha1
 from collections import namedtuple
 
-from vobject.base import ParseError, readOne
 
-from core.constants import HIGHER_EDUCATION_LEVELS, MBO_EDUCATIONAL_LEVELS
 from sources.utils.edurep import EdurepExtractor
-from sources.utils.sharekit import extract_channel, parse_url, extract_state, webhook_data_transformer
+from sources.utils.sharekit import parse_url
 from files.models import Set, FileDocument
 
 
 logger = logging.getLogger("harvester")
 
 FileInfo = namedtuple("FileInfo", ["product", "mime_type", "url"])
-
-
-
-def parse_vcard_element(record, external_id):
-    card = "\n".join(field.strip() for field in record.text.strip().split("\n"))
-    try:
-        return readOne(card)
-    except ParseError:
-        logger.warning(f"Can't parse vCard for material with id: {external_id}")
-        return
-
-def get_publishers(product, external_id):
-    publishers = []
-    publisher_element = product.find(string='publisher')
-    if not publisher_element:
-        return publishers
-    contribution_element = publisher_element.find_parent('czp:contribute')
-    if not contribution_element:
-        return publishers
-    nodes = contribution_element.find_all('czp:vcard')
-    for node in nodes:
-        publisher = parse_vcard_element(node, external_id)
-        if hasattr(publisher, "fn"):
-            publishers.append(publisher.fn.value)
-    return publishers
-
-def get_provider_name(product, external_id):
-    provider_name = None
-    publishers = get_publishers(product, external_id)
-    if len(publishers):
-        provider_name = publishers[0]
-    return provider_name
-
-
 
 
 def get_file_infos(edurep_soup) -> FileInfo:
@@ -58,8 +22,6 @@ def get_file_infos(edurep_soup) -> FileInfo:
             continue
         for mime_type, url in zip(mime_types, urls):
             yield FileInfo(product, mime_type, url)
-
-
 
 
 def get_file_seeds(edurep_soup):
@@ -76,7 +38,7 @@ def get_file_seeds(edurep_soup):
         product_id = product.find('identifier').text.strip()
         product_set = product.find('setSpec').text.strip()
         product_copyright = EdurepExtractor.find_all_classification_blocks(product, "access rights", "czp:id")
-        product_provider_name = get_provider_name(product, external_id=product_id)
+        product_provider_name = EdurepExtractor.get_provider_name(product, external_id=product_id)
         mime_types = product.find_all('czp:format')
         urls = product.find_all('czp:location')
         if not urls:
@@ -99,17 +61,17 @@ def get_file_seeds(edurep_soup):
         for mime_type, url, title in file_info_iterator:
             if not url:
                 continue
-            product_file = {}
-            product_file["state"] = EdurepExtractor.get_oaipmh_record_state(product)
-            product_file["set"] = product_set
+            product_file = {
+                "state": EdurepExtractor.get_oaipmh_record_state(product),
+                "set": product_set,
+                "product": {
+                    "provider": product_provider_name,
+                    "product_id": product_id,
+                    "copyright": product_copyright
+                },
+                "is_link": mime_type == "text/html"}
             # We add some product metadata, because unfortunately the product supplies defaults
-            product_file["product"] = {
-                "provider": product_provider_name,
-                "product_id": product_id,
-                "copyright": product_copyright
-            }
             # We indicate we're not dealing with a webpage URL
-            product_file["is_link"] = mime_type == "text/html"
             yield product_file
 
 
@@ -140,38 +102,42 @@ class EdurepFileExtraction(object):
         return info.product.find('setSpec').text.strip()
 
     @classmethod
-    def get_mime_type(cls, node: dict) -> str:
-        mime_type = node.get("resourceMimeType", None)
-        if mime_type is None and node.get("is_link", None):
-            mime_type = "text/html"
-        return mime_type
-
-    @classmethod
-    def get_access_rights(cls, node: dict) -> str | None:
-        access_rights = node.get("accessRight", None)
-        if not access_rights:
-            return
-        if access_rights[0].isupper():  # value according to standard; no parsing necessary
-            return access_rights
-        access_rights = access_rights.replace("access", "")
-        access_rights = access_rights.capitalize()
-        access_rights += "Access"
-        return access_rights
-
-    @classmethod
     def get_url(cls, soup, info: FileInfo) -> str | None:
         return parse_url(info.url.text.strip())
 
     @classmethod
     def get_mime_type(cls, soup, info: FileInfo) -> str | None:
-        return info.mime_type
+        return info.mime_type.text.strip()
 
     @classmethod
     def get_copyright(cls, soup, info: FileInfo) -> str | None:
         return EdurepExtractor.get_copyright(info.product)
 
+    @classmethod
+    def get_product_id(cls, soup, info: FileInfo) -> str | None:
+        return info.product.find('identifier').text.strip()
 
+    @classmethod
+    def get_access_rights(cls, soup, info: FileInfo) -> str | None:
+        default_access_rights = "ClosedAccess"
+        access_rights_blocks = EdurepExtractor.find_all_classification_blocks(info.product, "access rights", "czp:id")
+        if len(access_rights_blocks):
+            default_access_rights = access_rights_blocks[0].text.strip()
+        return default_access_rights
 
+    @classmethod
+    def get_is_link(cls, soup, info: FileInfo) -> bool | None:
+        return info.mime_type.text.strip() == "text/html"
+
+    @classmethod
+    def get_provider(cls, soup, info: FileInfo) -> dict | None:
+        provider_name = EdurepExtractor.get_provider_name(info.product, info.product.find('identifier').text.strip())
+        return {
+            "ror": None,
+            "external_id": None,
+            "slug": None,
+            "name": provider_name
+        }
 
 
 OBJECTIVE = {
@@ -180,16 +146,15 @@ OBJECTIVE = {
     "state": EdurepFileExtraction.get_state,
     "external_id": EdurepFileExtraction.get_hash,
     "set": EdurepFileExtraction.get_set,
-    # # Generic metadata
+    # Generic metadata
     "url": EdurepFileExtraction.get_url,
     "hash": EdurepFileExtraction.get_hash,
     "mime_type": EdurepFileExtraction.get_mime_type,
-    # "title": lambda node: node.get("fileName", node.get("urlName", None)),
     "copyright": EdurepFileExtraction.get_copyright,
-    # "access_rights": EdurepFileExtraction.get_access_rights,
-    # "product_id": lambda node: node["product"]["product_id"],
-    # "is_link": lambda node: node.get("is_link", None),
-    # "provider": lambda node: node["product"]["provider"]
+    "access_rights": EdurepFileExtraction.get_access_rights,
+    "product_id": EdurepFileExtraction.get_product_id,
+    "is_link": EdurepFileExtraction.get_is_link,
+    "provider": EdurepFileExtraction.get_provider
 }
 
 
@@ -218,6 +183,3 @@ SEEDING_PHASES = [
         "is_post_initialization": True
     }
 ]
-
-
-WEBHOOK_DATA_TRANSFORMER = webhook_data_transformer
