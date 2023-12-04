e24466700ef3e31d4b15ee3a3f102067f09a693b
Kalle Pronk <kallepronk@gmail.com>
2023-12-04 10:46:28 +0100

    remove unused edurep file extractor function

--- harvester/files/sources/edurep.py
+++ harvester/files/sources/edurep.py
@@ -23,57 +23,6 @@ def get_file_infos(edurep_soup) -> FileInfo:
             yield FileInfo(product, mime_type, url)
 
 
-def get_file_seeds(edurep_soup):
-    """
-    This function takes a Sharekit publication API response and transforms it into file dicts.
-    File dicts have at least an "url" key that indicates where we can find the file.
-    The "srn" key contains a unique identifier for the "url".
-    The "is_link" key indicates whether we should treat the "url" as an actual link instead of a file link.
-
-    :param edurep_soup: a parsed Sharekit publication API response
-    :return: yields file objects
-    """
-    for product in edurep_soup.find_all('record'):
-        product_id = product.find('identifier').text.strip()
-        product_set = product.find('setSpec').text.strip()
-        product_copyright = EdurepExtractor.find_all_classification_blocks(product, "access rights", "czp:id")
-        product_provider_name = EdurepExtractor.get_provider_name(product, external_id=product_id)
-        mime_types = product.find_all('czp:format')
-        urls = product.find_all('czp:location')
-        if not urls:
-            yield {
-                "url": None,
-                "state": "deleted",
-                "set": product_set,
-                "product": {
-                    "provider": product_provider_name,
-                    "product_id": product_id,
-                    "copyright": product_copyright
-                }
-            }
-            return
-        file_info_iterator = zip(
-            [mime_node.text.strip() for mime_node in mime_types],
-            [url_node.text.strip() for url_node in urls],
-            [f"URL {ix + 1}" for ix, mime_node in enumerate(mime_types)],
-        )
-        for mime_type, url, title in file_info_iterator:
-            if not url:
-                continue
-            product_file = {
-                "state": EdurepExtractor.get_oaipmh_record_state(product),
-                "set": product_set,
-                "product": {
-                    "provider": product_provider_name,
-                    "product_id": product_id,
-                    "copyright": product_copyright
-                },
-                "is_link": mime_type == "text/html"}
-            # We add some product metadata, because unfortunately the product supplies defaults
-            # We indicate we're not dealing with a webpage URL
-            yield product_file
-
-
 def back_fill_deletes(seed: dict, harvest_set: Set) -> Iterator[dict]:
     if not seed["state"] == FileDocument.States.DELETED.value:
         yield seed
