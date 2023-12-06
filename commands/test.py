cef1581bfefbcc94f70b75c1c71fb87b0ce90428
Fako Berkers <email@fakoberkers.nl>
2023-12-06 14:40:19 +0100

    Adds test_method option to test.run command to run fewer tests when desired

--- commands/test.py
+++ commands/test.py
@@ -4,18 +4,21 @@ from invoke import Collection
 
 @task(help={
     "test_file": "A path to a file containing a subset of tests to run",
+    "test_method": "An expression of which test methods to run",
     "warnings": "Whether to print warnings in the test report",
     "fast": "Whether to exclude tests marked as slow",
     "parallel": "Whether to run the tests in parallel on multiple cpus",
     "search_tests": "Only run the tests tagged with search",
     "fail_fast": "Fails at first failing test when enabled",
 })
-def run(ctx, test_file=None, warnings=False, fast=False, parallel=False, search_tests=False, fail_fast=False):
+def run(ctx, test_file=None, test_method=None, warnings=False, fast=False, parallel=False, search_tests=False,
+        fail_fast=False):
     """
     Runs the tests for the harvester
     """
     # Specify some flags we'll be passing on to pytest based on command line arguments
     test_file = test_file if test_file else ""
+    test_method_flag = f"-x -k {test_method}" if test_method else ""
     warnings_flag = "--disable-warnings" if not warnings else ""
     fail_fast_flag = "" if not fail_fast else "-x"
 
@@ -24,6 +27,7 @@ def run(ctx, test_file=None, warnings=False, fast=False, parallel=False, search_
     assert not fast or not parallel, \
         "Running --fast with --parallel is slower than just --fast due to multiprocessing overhead"
     assert not search_tests or not parallel, "Can't run search tests in parallel due to lack of index transactions"
+    assert not test_method or test_file, "Can't specify a test method without specifying the test file"
 
     # Determine value of marks flag which filters flags
     if test_file:
@@ -44,11 +48,14 @@ def run(ctx, test_file=None, warnings=False, fast=False, parallel=False, search_
     # Run pytest command
     with ctx.cd("harvester"):
         if not parallel:
-            ctx.run(f"pytest {test_file} {warnings_flag} {marks_flag} {fail_fast_flag}", echo=True, pty=True)
+            ctx.run(
+                f"pytest {test_file} {test_method_flag} {warnings_flag} {marks_flag} {fail_fast_flag}",
+                echo=True, pty=True
+            )
         else:
             # Running all tests in parallel except for search, because manipulating indices is not atomic
             ctx.run(
-                f"pytest {test_file} {warnings_flag} {marks_flag} {fail_fast_flag} -n auto --create-db",
+                f"pytest {warnings_flag} {marks_flag} {fail_fast_flag} -n auto --create-db",
                 echo=True, pty=True
             )
             ctx.run(f"pytest {warnings_flag} {fail_fast_flag} -m search --reuse-db", echo=True, pty=True)
