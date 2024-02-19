e684ecde0a45dda2b50cc991aaf94b77f8174f42
Fako Berkers <email@fakoberkers.nl>
2024-02-19 18:33:33 +0100

    Allows to drop to debugger from test.run command

--- commands/test.py
+++ commands/test.py
@@ -10,9 +10,10 @@ from invoke import Collection
     "parallel": "Whether to run the tests in parallel on multiple cpus",
     "search_tests": "Only run the tests tagged with search",
     "fail_fast": "Fails at first failing test when enabled",
+    "debug": "Whether to allow and stop at debugger statements"
 })
 def run(ctx, test_file=None, test_method=None, warnings=False, fast=False, parallel=False, search_tests=False,
-        fail_fast=False):
+        fail_fast=False, debug=False):
     """
     Runs the tests for the harvester
     """
@@ -21,6 +22,7 @@ def run(ctx, test_file=None, test_method=None, warnings=False, fast=False, paral
     test_method_flag = f"-x -k {test_method}" if test_method else ""
     warnings_flag = "--disable-warnings" if not warnings else ""
     fail_fast_flag = "" if not fail_fast else "-x"
+    debug_flag = "" if not debug else "-s"
 
     # Assert that inputs make sense
     assert not test_file or not parallel, "You shouldn't run tests from a single file in parallel"
@@ -28,6 +30,7 @@ def run(ctx, test_file=None, test_method=None, warnings=False, fast=False, paral
         "Running --fast with --parallel is slower than just --fast due to multiprocessing overhead"
     assert not search_tests or not parallel, "Can't run search tests in parallel due to lack of index transactions"
     assert not test_method or test_file, "Can't specify a test method without specifying the test file"
+    assert not debug or not parallel, "Can't run in parallel while debugging"
 
     # Determine value of marks flag which filters flags
     if test_file:
@@ -49,7 +52,7 @@ def run(ctx, test_file=None, test_method=None, warnings=False, fast=False, paral
     with ctx.cd("harvester"):
         if not parallel:
             ctx.run(
-                f"pytest {test_file} {test_method_flag} {warnings_flag} {marks_flag} {fail_fast_flag}",
+                f"pytest {test_file} {test_method_flag} {warnings_flag} {marks_flag} {fail_fast_flag} {debug_flag}",
                 echo=True, pty=True
             )
         else:
