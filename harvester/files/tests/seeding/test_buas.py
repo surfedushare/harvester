30e25c2ebcca2b7d47a83ab40ae9153baf4ab624
Fako Berkers <email@fakoberkers.nl>
2024-04-04 14:48:27 +0200

    Adds tests for BUAS file extraction

--- harvester/files/tests/seeding/test_buas.py
+++ harvester/files/tests/seeding/test_buas.py
@@ -7,7 +7,7 @@ from files.sources.buas import SEEDING_PHASES
 from sources.factories.buas.extraction import BuasPureResourceFactory
 
 
-class TestHanzeFileExtraction(TestCase):
+class TestBuasFileExtraction(TestCase):
 
     set = None
     seeds = []
@@ -31,6 +31,18 @@ class TestHanzeFileExtraction(TestCase):
             "cache_only": False
         })
 
+    def test_get_state(self):
+        self.assertEqual(self.seeds[0]["state"], "active")
+
+    def test_get_set(self):
+        self.assertEqual(self.seeds[0]["set"], "buas:buas")
+
+    def test_get_external_id(self):
+        self.assertEqual(
+            self.seeds[0]["external_id"],
+            "28aca36e-17b8-48eb-a4a3-70610dbf73f6:d42e0d5475f052d4fa0ef5216fd7dcbfc3a4374d"
+        )
+
     def test_get_url(self):
         self.assertEqual(
             self.seeds[0]["url"],
@@ -41,6 +53,29 @@ class TestHanzeFileExtraction(TestCase):
             "https://pure.buas.nl/ws/files/15672869/Peeters_tourismandclimatemitigation_peetersp_ed_nhtv2007.pdf"
         )
 
+    def test_get_hash(self):
+        self.assertEqual(self.seeds[0]["hash"], "d42e0d5475f052d4fa0ef5216fd7dcbfc3a4374d")
+
     def test_get_mime_type(self):
         self.assertEqual(self.seeds[0]["mime_type"], "text/html")
         self.assertEqual(self.seeds[2]["mime_type"], "application/pdf")
+
+    def test_get_title(self):
+        self.assertIsNone(self.seeds[0]["title"], "Expected links not to have titles")
+        self.assertEqual(
+            self.seeds[2]["title"],
+            "Peeters_tourismandclimatemitigation_peetersp_ed_nhtv2007.pdf"
+        )
+
+    def test_get_access_rights(self):
+        self.assertEqual(self.seeds[0]["access_rights"], "OpenAccess")
+
+    def test_get_product_id(self):
+        self.assertEqual(self.seeds[0]["product_id"], "28aca36e-17b8-48eb-a4a3-70610dbf73f6")
+
+    def test_get_is_link(self):
+        self.assertTrue(self.seeds[0]["is_link"])
+        self.assertFalse(self.seeds[2]["is_link"])
+
+    def test_get_provider(self):
+        self.assertEqual(self.seeds[0]["provider"], "buas")
