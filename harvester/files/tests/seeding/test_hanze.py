cc45a29b2972c41b72263179192b36841caae7df
Fako Berkers <email@fakoberkers.nl>
2024-04-04 10:41:01 +0200

    Adds tests for Hanze files

--- harvester/files/tests/seeding/test_hanze.py
+++ harvester/files/tests/seeding/test_hanze.py
@@ -32,6 +32,18 @@ class TestHanzeFileExtraction(TestCase):
             "cache_only": False
         })
 
+    def test_get_state(self):
+        self.assertEqual(self.seeds[0]["state"], "active")
+
+    def test_get_set(self):
+        self.assertEqual(self.seeds[0]["set"], "hanze:hanze")
+
+    def test_get_external_id(self):
+        self.assertEqual(
+            self.seeds[0]["external_id"],
+            "01ea0ee1-a419-42ee-878b-439b44562098:01df6be8b59f65074350ca33c8eded52ea106222"
+        )
+
     def test_get_url(self):
         self.assertEqual(
             self.seeds[0]["url"],
@@ -40,5 +52,28 @@ class TestHanzeFileExtraction(TestCase):
             "_met_ondergrondse_infiltratievoorzieningen_20_jaar.pdf"
         )
 
+    def test_get_hash(self):
+        self.assertEqual(self.seeds[0]["hash"], "01df6be8b59f65074350ca33c8eded52ea106222")
+
     def test_get_mime_type(self):
         self.assertEqual(self.seeds[0]["mime_type"], "application/pdf")
+
+    def test_get_title(self):
+        self.assertEqual(
+            self.seeds[0]["title"],
+            "wtnr2_verh1_p99_113_HR_v2_Inter_nationale_ervaringen_met_ondergrondse_infiltratievoorzieningen_20_jaar.pdf"
+        )
+
+    def test_get_access_rights(self):
+        self.assertEqual(self.seeds[0]["access_rights"], "OpenAccess")
+        self.assertEqual(self.seeds[2]["access_rights"], "RestrictedAccess")
+
+    def test_get_product_id(self):
+        self.assertEqual(self.seeds[0]["product_id"], "01ea0ee1-a419-42ee-878b-439b44562098")
+
+    def test_get_is_link(self):
+        self.assertFalse(self.seeds[0]["is_link"])
+        self.assertTrue(self.seeds[2]["is_link"])
+
+    def test_get_provider(self):
+        self.assertEqual(self.seeds[0]["provider"], "hanze")
