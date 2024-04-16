5c162209cb7d5791908f1f5c5f0eeb8c613bda65
Fako Berkers <email@fakoberkers.nl>
2024-04-16 14:58:57 +0200

    Adds test for invalidating nested tasks for FileDocument

---
+++ harvester/files/tests/models/test_document.py
@@ -0,0 +1,28 @@
+from copy import deepcopy
+
+from django.test import TestCase
+
+from files.models import FileDocument
+
+
+class DocumentTestCase(TestCase):
+
+    fixtures = ["test-document.json"]
+
+    def test_get_pending_tasks_hash_update(self):
+        # Check that file from fixture is not processing
+        file_document = FileDocument.objects.get(pk=1)
+        check_url_pipeline = deepcopy(file_document.pipeline["check_url"])
+        check_url_derivative = deepcopy(file_document.derivatives["check_url"])
+        self.assertEqual(file_document.get_pending_tasks(), [])
+        # Change the hash and see if the file becomes processing
+        file_document.update({"hash": "abc123"})
+        self.assertEqual(file_document.get_pending_tasks(), ["check_url"])
+        self.assertIsNotNone(file_document.pending_at)
+        self.assertIsNone(file_document.finished_at)
+        # Complete the check_url task by patching it and see if secondary tasks trigger
+        file_document.pipeline["check_url"] = check_url_pipeline
+        file_document.derivatives["check_url"] = check_url_derivative
+        self.assertEqual(file_document.get_pending_tasks(), ["tika"])
+        self.assertIsNotNone(file_document.pending_at)
+        self.assertIsNone(file_document.finished_at)
