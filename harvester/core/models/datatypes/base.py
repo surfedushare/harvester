a93dd9aed6dda0d7c882a43b4550da81674636c7
Fako Berkers <email@fakoberkers.nl>
2024-04-18 16:58:23 +0200

    Deletes resources that are tight to invalidated tasks

--- harvester/core/models/datatypes/base.py
+++ harvester/core/models/datatypes/base.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from datetime import datetime
 
+from django.apps import apps
 from django.db import models
 from django.utils.timezone import now
 
@@ -50,11 +51,22 @@ class HarvestObjectMixin(models.Model):
                     property_dependencies[dependency].append(task_name)
         return property_dependencies
 
+    def clear_task_result(self, task_name: str):
+        task_result = self.pipeline.get(task_name)
+        if task_result is None:
+            return
+        if (resource := task_result.get("resource")) and task_result.get("success"):
+            app_label, model_name = resource.split(".")
+            resource_model = apps.get_model(app_label, model_name)
+            resource_model.objects.filter(id=task_result.get("id")).delete()
+        del self.pipeline[task_name]
+        return
+
     def invalidate_task(self, task_name: str, current_time: datetime = None, commit: bool = False) -> None:
         is_invalidated = False
         if task_name in self.pipeline:
             is_invalidated = True
-            del self.pipeline[task_name]
+            self.clear_task_result(task_name)
         if task_name in self.derivatives:
             is_invalidated = True
             del self.derivatives[task_name]
