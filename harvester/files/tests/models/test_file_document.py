efb2ccde83b7431cba6e912815d5bc29dc388824
Fako Berkers <email@fakoberkers.nl>
2024-04-22 15:37:04 +0200

    Adds some resources to FileDocument fixture and checks purge behavour

--- harvester/files/tests/models/test_file_document.py
+++ harvester/files/tests/models/test_file_document.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 from django.test import TestCase
 
-from files.models import FileDocument
+from files.models import FileDocument, CheckURLResource, HttpTikaResource
 
 
 class FileDocumentTestCase(TestCase):
@@ -20,9 +20,23 @@ class FileDocumentTestCase(TestCase):
         self.assertEqual(file_document.get_pending_tasks(), ["check_url"])
         self.assertIsNotNone(file_document.pending_at)
         self.assertIsNone(file_document.finished_at)
+        # The Tika and URL resources should be cleared
+        self.assertEqual(CheckURLResource.objects.count(), 1, "Expected check_url resource for file to get purged")
+        self.assertEqual(HttpTikaResource.objects.count(), 1, "Expected tika resources for file to get purged")
         # Complete the check_url task by patching it and see if secondary tasks trigger
         file_document.pipeline["check_url"] = check_url_pipeline
         file_document.derivatives["check_url"] = check_url_derivative
         self.assertEqual(file_document.get_pending_tasks(), ["tika"])
         self.assertIsNotNone(file_document.pending_at)
         self.assertIsNone(file_document.finished_at)
+
+    def test_invalidate_task_resource_purge(self):
+        # Pre-test asserts
+        self.assertEqual(CheckURLResource.objects.count(), 2, "Expected two check_url resources at start of test")
+        self.assertEqual(HttpTikaResource.objects.count(), 2, "Expected two tika resources at start of test")
+        file_document = FileDocument.objects.get(pk=1)
+        file_document.invalidate_task("check_url")
+        self.assertIsNotNone(file_document.pending_at)
+        self.assertIsNone(file_document.finished_at)
+        self.assertEqual(CheckURLResource.objects.count(), 1, "Expected check_url resource from pipeline to be deleted")
+        self.assertEqual(HttpTikaResource.objects.count(), 2, "Expected tika resources to be unaffected")
