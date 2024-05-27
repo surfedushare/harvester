650a5fc96e46beeafc2ef3ac237427b417ed5575
Fako Berkers <email@fakoberkers.nl>
2024-05-27 15:46:37 +0200

    Drastically simplifies the core admin app for easier documentation

--- harvester/core/admin/__init__.py
+++ harvester/core/admin/__init__.py
@@ -1,27 +1,10 @@
 from django.contrib import admin
 
-from datagrowth.admin import HttpResourceAdmin, ShellResourceAdmin
+from datagrowth.admin import HttpResourceAdmin
 
-from core.models import (Dataset, DatasetVersion, Collection, Document, HarvestSource, HttpTikaResource, ElasticIndex,
-                         ExtructResource, Query, Extension, MatomoVisitsResource)
-from core.models import YoutubeThumbnailResource, PdfThumbnailResource
-from core.admin.legacy_datatypes import (DatasetAdmin, DatasetVersionAdmin, DocumentAdmin, ExtensionAdmin,
-                                         CollectionAdmin)
-from core.admin.harvest import HarvestSourceAdmin
-from core.admin.search import ElasticIndexAdmin
+from core.models import Query, MatomoVisitsResource
 from core.admin.query import QueryAdmin
 
 
-admin.site.register(HarvestSource, HarvestSourceAdmin)
-admin.site.register(HttpTikaResource, HttpResourceAdmin)
-admin.site.register(Dataset, DatasetAdmin)
-admin.site.register(DatasetVersion, DatasetVersionAdmin)
-admin.site.register(Collection, CollectionAdmin)
-admin.site.register(Document, DocumentAdmin)
-admin.site.register(ElasticIndex, ElasticIndexAdmin)
-admin.site.register(ExtructResource, HttpResourceAdmin)
 admin.site.register(MatomoVisitsResource, HttpResourceAdmin)
-admin.site.register(Extension, ExtensionAdmin)
-admin.site.register(YoutubeThumbnailResource, ShellResourceAdmin)
-admin.site.register(PdfThumbnailResource, HttpResourceAdmin)
 admin.site.register(Query, QueryAdmin)
