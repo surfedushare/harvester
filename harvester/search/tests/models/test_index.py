df9bcbc4a8fa10c8fef8834a8e8360e389bf2c01
Fako Berkers <email@fakoberkers.nl>
2024-07-18 10:01:46 +0200

    Improves tests for creating and updating OpenSearchIndex and its indices

---
+++ harvester/search/tests/models/test_index.py
@@ -0,0 +1,43 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from search.models import OpenSearchIndex
+from search.tests.mocks import get_search_client_mock
+
+
+class TestOpenSearchIndexModel(TestCase):
+
+    search_client = get_search_client_mock(has_history=True)
+
+    def setUp(self):
+        super().setUp()
+        self.search_client.indices.create.reset_mock()
+        self.search_client.indices.delete.reset_mock()
+
+    def test_build(self):
+        instance = OpenSearchIndex.build("testing", "test", "0.0.1")
+        self.assertIsNone(instance.id)
+        self.assertEqual(instance.name, "edusources-testing--test-0.0.1")
+        self.assertEqual(set(instance.configuration.keys()), {"nl", "en", "unk"})
+
+    @patch("search.models.index.get_opensearch_client", return_value=search_client)
+    def test_delete(self, get_search_client_mock):
+        instance = OpenSearchIndex.build("testing", "test", "0.0.1")
+        instance.save()
+        instance.delete()  # we're testing this
+        for language in ["en", "nl", "unk"]:
+            self.search_client.indices.delete.assert_any_call(
+                index=f"edusources-testing--test-001-{language}"
+            )
+
+    def test_get_remote_names(self):
+        instance = OpenSearchIndex.build("testing", "test", "0.0.1")
+        self.assertRaises(ValueError, instance.get_remote_names)
+        instance.save()
+        names = instance.get_remote_names()
+        self.assertEqual(set(names), {
+            "edusources-testing--test-001-en",
+            "edusources-testing--test-001-nl",
+            "edusources-testing--test-001-unk"
+        })
