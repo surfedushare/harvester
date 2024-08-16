2bfc3b3e1ad0f120e0f58387ff07c6398c0a4b66
Fako Berkers <email@fakoberkers.nl>
2024-08-16 19:14:59 +0200

    Uses OpenSearchClientBuilder when getting client for invoke commands

--- commands/opensearch/utils.py
+++ commands/opensearch/utils.py
@@ -1,4 +1,4 @@
-from opensearchpy import OpenSearch, RequestsHttpConnection
+from search_client.opensearch import OpenSearchClientBuilder
 
 
 def get_remote_search_client(conn, silent=False):
@@ -6,23 +6,5 @@ def get_remote_search_client(conn, silent=False):
     Returns the Open Search client connected through port forwarding settings
     """
     host = conn.config.opensearch.host
-    protocol_config = {
-        "scheme": "https",
-        "port": 443,
-        "use_ssl": True,
-        "verify_certs": True,
-    }
-
     http_auth = ("supersurf", conn.config.secrets.opensearch.password,)
-
-    es_client = OpenSearch(
-        [host],
-        http_auth=http_auth,
-        connection_class=RequestsHttpConnection,
-        **protocol_config
-    )
-
-    # test if it works
-    if not silent and not es_client.cat.health(request_timeout=30):
-        raise ValueError('Credentials do not work for Open Search')
-    return es_client
+    return OpenSearchClientBuilder.from_host(host, http_auth=http_auth).build(check_connection=not silent)
