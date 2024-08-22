65c450d40f006bf8c2197f15b62fb8043b3a5b65
Fako Berkers <email@fakoberkers.nl>
2024-08-22 16:16:36 +0200

    Checks DRF conversion for all factories of search-client

--- harvester/testing/tests/backward_compatibility/test_pydantic_to_drf.py
+++ harvester/testing/tests/backward_compatibility/test_pydantic_to_drf.py
@@ -3,7 +3,8 @@ import json
 from django.test import TestCase
 from rest_framework.exceptions import ValidationError
 
-from search_client.test.factories import generate_nl_material
+from search_client.constants import Platforms
+from search_client.test.factories import generate_nl_material, generate_nl_product, generate_material, generate_product
 from search_client.serializers.products import LearningMaterial, ResearchProduct
 
 from products.views.serializers import SimpleLearningMaterialResultSerializer, ResearchProductResultSerializer
@@ -11,24 +12,39 @@ from products.views.serializers import SimpleLearningMaterialResultSerializer, R
 
 class TestPydanticToDRFConversion(TestCase):
 
-    def test_learning_material_math(self):
-        data = generate_nl_material(topic="math")
-        learning_material = LearningMaterial(**data)
-        learning_material_json = learning_material.model_dump_json()
-        untyped_learning_material = json.loads(learning_material_json)
-        serializer = SimpleLearningMaterialResultSerializer(data=untyped_learning_material)
-        try:
-            serializer.is_valid()
-        except ValidationError as exc:
-            self.fail(f"SimpleLearningMaterialResultSerializer raised validation error: {exc}")
+    factories = {
+        Platforms.EDUSOURCES: [generate_nl_material, generate_material],
+        Platforms.PUBLINOVA: [generate_nl_product, generate_product]
+    }
 
-    def test_research_product_math(self):
-        data = generate_nl_material(topic="math")
-        research_product = ResearchProduct(**data)
-        research_product_json = research_product.model_dump_json()
-        untyped_research_product = json.loads(research_product_json)
-        serializer = ResearchProductResultSerializer(data=untyped_research_product)
-        try:
-            serializer.is_valid()
-        except ValidationError as exc:
-            self.fail(f"ResearchProductResultSerializer raised validation error: {exc}")
+    def test_edusources(self):
+        for factory in self.factories[Platforms.EDUSOURCES]:
+            for topic in ["math", "biology"]:
+                data = factory(topic=topic)
+                learning_material = LearningMaterial(**data)
+                learning_material_json = learning_material.model_dump_json()
+                untyped_learning_material = json.loads(learning_material_json)
+                serializer = SimpleLearningMaterialResultSerializer(data=untyped_learning_material)
+                try:
+                    serializer.is_valid()
+                except ValidationError as exc:
+                    self.fail(
+                        f"SimpleLearningMaterialResultSerializer raised validation error "
+                        f"with {topic} using factory {factory.__name__}: {exc}"
+                    )
+
+    def test_publinova(self):
+        for factory in self.factories[Platforms.PUBLINOVA]:
+            for topic in ["math", "biology"]:
+                data = generate_nl_material(topic=topic)
+                research_product = ResearchProduct(**data)
+                research_product_json = research_product.model_dump_json()
+                untyped_research_product = json.loads(research_product_json)
+                serializer = ResearchProductResultSerializer(data=untyped_research_product)
+                try:
+                    serializer.is_valid()
+                except ValidationError as exc:
+                    self.fail(
+                        f"ResearchProductResultSerializer raised validation error "
+                        f"with {topic} using factory {factory.__name__}: {exc}"
+                    )
