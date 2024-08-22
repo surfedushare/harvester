6ed3766e1281f569b77f634c24de6da9e5d95024
Fako Berkers <email@fakoberkers.nl>
2024-08-22 17:18:26 +0200

    Rewrites the OpenSearch test mixin to use the search_client.test.cases mixin

--- harvester/search/tests/views/base.py
+++ harvester/search/tests/views/base.py
@@ -1,81 +1,39 @@
 from django.conf import settings
 from django.test import TestCase, tag
 
+from opensearchpy import OpenSearch
+
 from search_client.opensearch import OpenSearchClientBuilder
-from search_client.constants import LANGUAGES, DocumentTypes
-from search_client.opensearch.indices.legacy import create_open_search_index_configuration
-from search_client.test.factories import generate_nl_material, generate_nl_product
+from search_client.constants import Entities, Platforms
+from search_client.test.cases import SearchClientIntegrationTestCaseMixin
 
 
 @tag("search")
-class OpenSearchTestCaseMixin:
+class OpenSearchTestCaseMixin(SearchClientIntegrationTestCaseMixin):
 
-    search = None
-    instance = None
-    document_type = None
-    alias_prefix = "test"
+    platform = None  # should be set on inheriting classes
 
     @classmethod
-    def index_body(cls, language):
-        return create_open_search_index_configuration(language, DocumentTypes.LEARNING_MATERIAL)
-
-    @classmethod
-    def index_document(cls, document_type, is_last_document=False, **kwargs):
-        match document_type:
-            case DocumentTypes.LEARNING_MATERIAL:
-                generate_document = generate_nl_material
-            case DocumentTypes.RESEARCH_PRODUCT:
-                generate_document = generate_nl_product
-            case _:
-                raise ValueError(f"Invalid document type to index_document: {document_type}")
-        body = generate_document(**kwargs)
-        index_kwargs = {
-            "id": body["srn"],
-            "index": cls.get_alias("nl"),
-            "body": body
-        }
-        if is_last_document:
-            index_kwargs["refresh"] = True
-        cls.search.index(**index_kwargs)
-
-    @classmethod
-    def get_alias(cls, language):
-        return f"{cls.alias_prefix}-{settings.PLATFORM.value}-{language}"
+    def setup_opensearch_client(cls) -> OpenSearch:
+        return OpenSearchClientBuilder.from_host(settings.OPENSEARCH_HOST).build()
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Setup indices
-        cls.search = OpenSearchClientBuilder.from_host(settings.OPENSEARCH_HOST).build()
-        for language in LANGUAGES:
-            cls.search.indices.create(
-                cls.get_alias(language),
-                ignore=400,
-                body=cls.index_body('nl')
-            )
         # Add some mock data to the indices
-        cls.index_document(cls.document_type)
+        cls.index_document(Entities.PRODUCTS)
         cls.index_document(
-            cls.document_type, is_last_document=True,
-            source="surfsharekit", external_id="abc", title=f"Nog een {cls.document_type.value}",
+            Entities.PRODUCTS, is_last_entity_document=True,
+            source="surfsharekit", external_id="abc", title=f"Nog een {Entities.PRODUCTS.value}",
             publisher_date="2020-03-18"
         )
-
-    @classmethod
-    def tearDownClass(cls):
-        for language in LANGUAGES:
-            cls.search.indices.delete(
-                cls.get_alias(language)
-            )
-        cls.search.close()
-        super().tearDownClass()
 
 
 class DocumentAPITestCase(TestCase):
 
-    document_type = None
+    platform = None
     required_properties = {
-        DocumentTypes.LEARNING_MATERIAL: [
+        Platforms.EDUSOURCES: [
             "title",
             "url",
             "files",
@@ -96,7 +54,7 @@ class DocumentAPITestCase(TestCase):
             "technical_type",
             "disciplines",
         ],
-        DocumentTypes.RESEARCH_PRODUCT: [
+        Platforms.PUBLINOVA: [
             "title",
             "url",
             "files",
@@ -121,5 +79,5 @@ class DocumentAPITestCase(TestCase):
     }
 
     def assert_result_properties(self, result):
-        for property_ in self.required_properties[self.document_type]:
+        for property_ in self.required_properties[self.platform]:
             self.assertIn(property_, result)
