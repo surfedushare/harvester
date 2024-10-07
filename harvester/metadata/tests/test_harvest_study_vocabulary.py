de040894524657a27f50976576cb210039697726
Fako Berkers <email@fakoberkers.nl>
2024-10-07 14:07:32 +0200

    Makes the study vocabulary resource fixtures a bit smaller for maintenance

--- harvester/metadata/tests/test_harvest_study_vocabulary.py
+++ harvester/metadata/tests/test_harvest_study_vocabulary.py
@@ -1,16 +1,16 @@
 from unittest.mock import patch
 
-from django.test import TestCase, tag
+from django.test import TestCase
 from django.core.management import call_command
 
 from datagrowth.configuration import register_defaults
+from datagrowth.resources.testing import ResourceFixturesMixin
 from metadata.models import MetadataValue
 
 
-@tag("slow")
-class TestHarvestStudyVocabulary(TestCase):
+class TestHarvestStudyVocabulary(ResourceFixturesMixin, TestCase):
 
-    fixtures = ["initial-study-vocabulary-resources"]
+    resource_fixtures = ["study-vocabulary-resources"]
 
     @classmethod
     def setUpClass(cls):
@@ -28,30 +28,31 @@ class TestHarvestStudyVocabulary(TestCase):
 
     @patch("metadata.management.commands.harvest_study_vocabulary.translate_with_deepl", return_value="Translated")
     def test_no_duplicates(self, fake_deepl):
-        call_command("harvest_study_vocabulary", "--vocabulary=verpleegkunde")
-        call_command("harvest_study_vocabulary", "--vocabulary=verpleegkunde")
+        call_command("harvest_study_vocabulary", "--vocabulary=informatievaardigheid")
+        call_command("harvest_study_vocabulary", "--vocabulary=informatievaardigheid")
         total_objects = MetadataValue.objects.count()
-        self.assertEqual(total_objects, 390,
+        self.assertEqual(total_objects, 33,
                          "When the command runs twice it should not duplicate values.")
 
     @patch("metadata.management.commands.harvest_study_vocabulary.translate_with_deepl", return_value="Translated")
     def test_same_number_applied_science(self, fake_deepl):
-        with self.assertNumQueries(877):
-            call_command("harvest_study_vocabulary", "--vocabulary=applied-science")
+        with self.assertNumQueries(172):
+            call_command("harvest_study_vocabulary", "--vocabulary=informatievaardigheid")
 
     @patch("metadata.management.commands.harvest_study_vocabulary.translate_with_deepl", return_value="Translated")
     def test_data_contains_right_values(self, fake_deepl):
-        call_command("harvest_study_vocabulary", "--vocabulary=applied-science")
+        call_command("harvest_study_vocabulary", "--vocabulary=informatievaardigheid")
         value = MetadataValue.objects.get(
-            value="http://purl.edustandaard.nl/concept/27aee99f-1b5f-45ba-84e9-4a52c1d46a63")
-        self.assertEqual(value.name, "Python")
-        self.assertEqual(value.parent.value,
-                         "http://purl.edustandaard.nl/concept/982e3b48-90b9-4fbd-9365-04289afe6929")
+            value="http://purl.edustandaard.nl/concept/598b9817-a153-400e-8710-78acadf70cbe")
+        self.assertEqual(value.name, "Plannen en zoeken")
+        self.assertEqual(value.parent.value, "informatievaardigheid")
         self.assertEqual(
-            [descendant.name for descendant in value.get_descendants()],
+            sorted([descendant.name for descendant in value.get_descendants()]),
             [
-                "NumPy", "Biopython", "Pandas", "Statistics with Python", "Python Basics",
-                "Data Visualisation with Python", "Data Cleaning with Python", "Machine Learning  with Python", "SciPy"
+
+                "Selecteren informatiebronnen en zoeksystemen",
+                "Selecteren zoektermen",
+                "Zoeken naar informatie",
             ]
         )
         self.assertTrue(value.is_manual, "Expected values to be manual to prevent automatic deletion")
@@ -62,4 +63,4 @@ class TestHarvestStudyVocabulary(TestCase):
         self.assertTrue(value.translation.nl)
         self.assertTrue(value.translation.en)
         self.assertTrue(value.translation.is_fuzzy)
-        self.assertEqual(fake_deepl.call_count, 174)
+        self.assertEqual(fake_deepl.call_count, 33)
