f4d770f13f3613d3219137d81c6ca62bb4dc2e6b
Fako Berkers <email@fakoberkers.nl>
2024-10-24 19:09:48 +0200

    Adds product overwrites to the admin

--- harvester/products/admin.py
+++ harvester/products/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
+from datagrowth.admin import DocumentAdmin as OverwriteAdmin
 from core.admin.datatypes import DatasetAdmin, DatasetVersionAdmin, SetAdmin, DocumentAdmin
 from core.admin.harvest import HarvestStateAdmin
-from products.models import Dataset, DatasetVersion, Set, ProductDocument, HarvestState
+from products.models import Dataset, DatasetVersion, Set, ProductDocument, HarvestState, Overwrite
 
 
 admin.site.register(Dataset, DatasetAdmin)
 admin.site.register(DatasetVersion, DatasetVersionAdmin)
 admin.site.register(Set, SetAdmin)
 admin.site.register(ProductDocument, DocumentAdmin)
+admin.site.register(Overwrite, OverwriteAdmin)
 
 admin.site.register(HarvestState, HarvestStateAdmin)
