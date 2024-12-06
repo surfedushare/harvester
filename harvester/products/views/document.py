2315779457318a341d6e6504e0595a62ed31ec26
Fako Berkers <email@fakoberkers.nl>
2024-12-06 15:26:39 +0100

    Adds serializers and strips defaults from product serializer to improve API documentation

--- harvester/products/views/document.py
+++ harvester/products/views/document.py
@@ -78,26 +78,6 @@ class SearchProductListView(SearchDocumentListViewMixin, DatasetVersionDocumentL
     When using the ``modified_since`` parameter the products will be limited to products that have been modified
     by this service since that date. Note that this service may decide to refresh data,
     even though sources didn't change that data.
-
-    Most properties for a ProductDocument are automatically documented through the interactive documentation.
-    However there are a few special properties that we'll document here.
-
-    **authors**: The list of authors for a ProductDocument.
-    Authors are objects with the following properties: name, email, external_id, dai, orcid and isni.
-    All these properties have string values or are null.
-
-    **files**: The list of files for a ProductDocument.
-    Files are objects with the following properties: title, url, mime_type, hash, copyright and access_rights.
-    All these properties have string values or are null.
-    The hash of a file is a sha1 hash of the file URL and doesn't represent the content of a file.
-    The copyright and access_rights properties have values as defined in the NL-LOM standard.
-
-    **previews**: For a product that supports thumbnails this object will contain three image links:
-    preview, full_size and preview_small. Where preview is 400x300 pixels and small_preview 200x150 pixels in size.
-    The full_size image varies in size, but will be the largest possible image.
-
-    **video**: For a product with a video that supports additional metadata this object will contain
-    the duration and the embed_url of that video
     """
     entity = Entities.PRODUCTS
     exclude_deletes_unless_modified_since_filter = True
@@ -108,26 +88,6 @@ class SearchProductDetailView(SearchDocumentRetrieveViewMixin, DatasetVersionDoc
     Returns the most recent version of a product, using its SURF Resource Name as an identifier,
     in the same format a search result would return it.
     This is useful if a system wants to update their copy of a product.
-
-    Most properties for a ProductDocument are automatically documented through the interactive documentation.
-    However there are a few special properties that we'll document here.
-
-    **authors**: The list of authors for a ProductDocument.
-    Authors are objects with the following properties: name, email, external_id, dai, orcid and isni.
-    All these properties have string values or are null.
-
-    **files**: The list of files for a ProductDocument.
-    Files are objects with the following properties: title, url, mime_type, hash, copyright and access_rights.
-    All these properties have string values or are null.
-    The hash of a file is a sha1 hash of the file URL and doesn't represent the content of a file.
-    The copyright and access_rights properties have values as defined in the NL-LOM standard.
-
-    **previews**: For a product that supports thumbnails this object will contain three image links:
-    preview, full_size and preview_small. Where preview is 400x300 pixels and small_preview 200x150 pixels in size.
-    The full_size image varies in size, but will be the largest possible image.
-
-    **video**: For a product with a video that supports additional metadata this object will contain
-    the duration and the embed_url of that video
     """
     entity = Entities.PRODUCTS
     exclude_deletes_unless_modified_since_filter = True
--- harvester/products/views/serializers/base.py
+++ harvester/products/views/serializers/base.py
@@ -1,6 +1,68 @@
 from rest_framework import serializers
 
 
+class VideoSerializer(serializers.Serializer):
+    embed_url = serializers.URLField()
+    duration = serializers.CharField()
+
+
+class PreviewsSerializer(serializers.Serializer):
+    full_size = serializers.URLField()
+    preview = serializers.URLField()
+    preview_small = serializers.URLField()
+
+
+class FileSerializer(serializers.Serializer):
+    srn = serializers.CharField()
+    hash = serializers.CharField()
+    access_rights = serializers.CharField()
+    state = serializers.ChoiceField(
+        choices=['active', 'deleted', 'archived'],
+        default='active'
+    )
+    is_link = serializers.BooleanField(default=False)
+    url = serializers.URLField(allow_null=True, default=None)
+    type = serializers.CharField(allow_null=True, default=None)
+    title = serializers.CharField(allow_null=True, default=None)
+    copyright = serializers.CharField(allow_null=True, default=None)
+    mime_type = serializers.CharField(allow_null=True, default=None)
+    video = VideoSerializer(allow_null=True, default=None)
+    previews = PreviewsSerializer(allow_null=True, default=None)
+    priority = serializers.IntegerField(default=0)
+
+
+class AuthorSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    email = serializers.EmailField(allow_null=True, default=None)
+    external_id = serializers.CharField(
+        allow_null=True,
+        default=None,
+        help_text="The id of the author in the source system."
+    )
+    dai = serializers.CharField(allow_null=True, default=None)
+    isni = serializers.CharField(allow_null=True, default=None)
+    orcid = serializers.CharField(allow_null=True, default=None)
+    is_external = serializers.BooleanField(
+        allow_null=True,
+        default=None,
+        help_text="This means external author for the Provider of a Product."
+    )
+
+
+class StarRatingsSerializer(serializers.Serializer):
+    average = serializers.FloatField(default=0.0)
+    star_1 = serializers.IntegerField(default=0)
+    star_2 = serializers.IntegerField(default=0)
+    star_3 = serializers.IntegerField(default=0)
+    star_4 = serializers.IntegerField(default=0)
+    star_5 = serializers.IntegerField(default=0)
+
+
+class MetricsSerializer(serializers.Serializer):
+    views = serializers.IntegerField(default=0)
+    stars = StarRatingsSerializer()
+
+
 class BaseSearchResultSerializer(serializers.Serializer):
 
     entity = serializers.CharField()
@@ -17,12 +79,12 @@ class BaseSearchResultSerializer(serializers.Serializer):
     description = serializers.CharField(allow_null=True, allow_blank=True)
     language = serializers.CharField(allow_null=True)
     copyright = serializers.CharField(allow_null=True)
-    video = serializers.DictField(default=None, allow_null=True)
+    video = VideoSerializer(allow_null=True, default=None)
     harvest_source = serializers.CharField()
-    previews = serializers.DictField(default=None, allow_null=True)
-    files = serializers.ListField(child=serializers.DictField())
-    authors = serializers.ListField(child=serializers.DictField())
+    previews = PreviewsSerializer(allow_null=True, default=None)
+    files = FileSerializer(many=True)
+    authors = AuthorSerializer(many=True)
     has_parts = serializers.ListField(child=serializers.CharField())
     is_part_of = serializers.ListField(child=serializers.CharField())
     keywords = serializers.ListField(child=serializers.CharField())
-    metrics = serializers.DictField(default=None, allow_null=True)
+    metrics = MetricsSerializer(allow_null=True)
