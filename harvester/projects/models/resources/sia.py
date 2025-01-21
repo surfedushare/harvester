7ed945ff298768f6b004497db180312d02410608
Fako Berkers <email@fakoberkers.nl>
2025-01-21 11:30:55 +0100

    Uses updatessinds for SIA project ids

--- harvester/projects/models/resources/sia.py
+++ harvester/projects/models/resources/sia.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.conf import settings
 
 from datagrowth.resources import HttpResource
@@ -5,10 +7,26 @@ from datagrowth.resources import HttpResource
 
 class SiaProjectIdsResource(HttpResource):
 
-    URI_TEMPLATE = settings.SOURCES["sia"]["endpoint"] + "/v1/projecten"
+    URI_TEMPLATE = settings.SOURCES["sia"]["endpoint"] + "/v1/projecten?updatessinds={}"
     HEADERS = {
         "accept": "application/json"
     }
+
+    def variables(self, *args):
+        # We need to parse ISO inputs to SIA's own special datetime format.
+        # Parse variables as normal and use variables used to fill in the URI_TEMPLATE.
+        variables = super().variables(*args)
+        url_variables = variables["url"]
+        # If we don't receive datetime data from inputs we use 1970-01-01 as default harvesting start date.
+        try:
+            iso_updated_sinds = url_variables[1]
+        except IndexError:
+            iso_updated_sinds = "1970-01-01T01:20:38Z"
+        # Transform the ISO standard into SIA's special format
+        updated_since = datetime.fromisoformat(iso_updated_sinds.replace('Z', '+00:00'))
+        return {
+            "url": [updated_since.strftime("%Y%m%d%H%M%S")]  # see SIA documentation about format
+        }
 
     def auth_headers(self):
         return {
