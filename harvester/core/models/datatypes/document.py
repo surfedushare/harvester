55665a9c505470561251ca120a2374359457ce71
Fako Berkers <email@fakoberkers.nl>
2025-01-23 17:46:48 +0100

    Checks first_processed_at date before retrying tasks

--- harvester/core/models/datatypes/document.py
+++ harvester/core/models/datatypes/document.py
@@ -4,7 +4,7 @@ import json
 from hashlib import sha1
 from sentry_sdk import capture_message
 from operator import xor
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.db import models
 from django.core.serializers.json import DjangoJSONEncoder
@@ -158,7 +158,11 @@ class HarvestDocument(DocumentBase, HarvestObjectMixin):
     def prepare_task_processing(self, current_time: datetime) -> None:
         # Check if previous runs have any errors and invalidate tasks where errors have occurred
         for task, result in list(self.pipeline.items()):
-            if not result.get("success", False) and task != "check_url":
+            first_processed_at = result.get("first_processed_at")
+            if first_processed_at:
+                first_processed_at = datetime.fromisoformat(first_processed_at)
+            if not result.get("success", False) and task != "check_url" and \
+                    (first_processed_at is None or first_processed_at >= (current_time - timedelta(days=3))):
                 self.invalidate_task(task, current_time=current_time)
         # Check if any open tasks are left and start processing if that is the case
         if self.state == self.States.ACTIVE and self.get_pending_tasks():
