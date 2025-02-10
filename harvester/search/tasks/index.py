f20de486ef985a5d26885d00d9755f8f1dcbe876
Fako Berkers <email@fakoberkers.nl>
2025-02-10 17:26:57 +0100

    Revert "Introduces a small delay when pushing to Open Search to prevent 429's"
    This reverts commit 3be147afae2bf0c34b3215caecfb9bf700d554ca.

--- harvester/search/tasks/index.py
+++ harvester/search/tasks/index.py
@@ -1,4 +1,3 @@
-from time import sleep
 from datetime import datetime
 
 from django.apps import apps
@@ -42,7 +41,6 @@ def _push_dataset_version_to_index(dataset_version: HarvestDatasetVersion, logge
                         search_document_batch.append((language, document.to_search(use_multilingual_fields=False)))
                     search_document_batch.append(("all", document.to_search(use_multilingual_fields=True)))
                 errors += index.push(search_document_batch, is_done=False)
-                sleep(0.1)
             # All documents have been pushed. We'll mark the push as done.
             index.pushed_at = current_time
             index.save()
