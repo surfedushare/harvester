77b2cb21094a49eedcf02993070ea761ffebfca0
Fako Berkers <email@fakoberkers.nl>
2025-04-01 10:19:51 +0200

    Removes acc.sharekit connection for acc.edusources

--- harvester/harvester/settings/edusources_nl.py
+++ harvester/harvester/settings/edusources_nl.py
@@ -9,9 +9,3 @@ SHAREKIT_TEST_ORGANIZATIONS = ["SURF edusources test"]
 SIMPLE_METADATA_FREQUENCY_FIELDS = ["study_vocabulary"]
 
 SET_PRODUCT_COPYRIGHT_BY_MAIN_FILE_COPYRIGHT = True
-
-# This is a temporary hard coded override that's hard to achieve with environment variables alone.
-# Can be removed if Edusources no longer wants to test OERWizard on Sharekit acceptance.
-# Changing this setting in this file means it only propagates to Edusources and not Publinova or MBO.
-if MODE == "acceptance":
-    SOURCES["sharekit"]["endpoint"] = "https://api.acc.surfsharekit.nl"
