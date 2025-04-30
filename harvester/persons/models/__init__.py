ce4b1f78bc1cb43f7766e8a9d5ec4de7b07c6e60
Fako Berkers <email@fakoberkers.nl>
2025-04-30 17:21:08 +0200

    Adds BUAS persons harvesting

--- harvester/persons/models/__init__.py
+++ harvester/persons/models/__init__.py
@@ -9,3 +9,4 @@ from persons.models.resources.publinova import PublinovaPersonResource
 from persons.models.resources.hanze import HanzePersonResource, HanzeUserResource
 from persons.models.resources.hva import HvaPersonResource, HvAUserResource
 from persons.models.resources.hu import HuPersonResource
+from persons.models.resources.buas import BuasPersonResource
