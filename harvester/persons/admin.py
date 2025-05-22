fc0e6b73070df03c6d816a3ce4a5679056c669dc
Fako Berkers <email@fakoberkers.nl>
2025-05-22 10:33:14 +0200

    Adds person admin models for Resources

--- harvester/persons/admin.py
+++ harvester/persons/admin.py
@@ -5,7 +5,8 @@ from core.admin.datatypes import DatasetAdmin, DatasetVersionAdmin, SetAdmin, Do
 from core.admin.harvest import HarvestStateAdmin
 
 from persons.models import (Dataset, DatasetVersion, Set, PersonDocument, HarvestState, HkuPersonResource,
-                            PublinovaPersonResource)
+                            PublinovaPersonResource, HanzePersonResource, HanzeUserResource, HvaPersonResource,
+                            HvAUserResource, HuPersonResource, BuasPersonResource)
 
 
 admin.site.register(Dataset, DatasetAdmin)
@@ -17,3 +18,9 @@ admin.site.register(HarvestState, HarvestStateAdmin)
 
 admin.site.register(HkuPersonResource, HttpResourceAdmin)
 admin.site.register(PublinovaPersonResource, HttpResourceAdmin)
+admin.site.register(HanzePersonResource, HttpResourceAdmin)
+admin.site.register(HanzeUserResource, HttpResourceAdmin)
+admin.site.register(HvaPersonResource, HttpResourceAdmin)
+admin.site.register(HvAUserResource, HttpResourceAdmin)
+admin.site.register(HuPersonResource, HttpResourceAdmin)
+admin.site.register(BuasPersonResource, HttpResourceAdmin)
