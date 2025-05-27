b44d5f296ea38e9c95f197ce6a5c4f6e1c6422cc
Fako Berkers <email@fakoberkers.nl>
2025-05-27 11:23:53 +0200

    Expands Edusources update tests to strict multilingual fields

--- harvester/harvester/settings/edusources_nl.py
+++ harvester/harvester/settings/edusources_nl.py
@@ -13,3 +13,4 @@ SET_PRODUCT_COPYRIGHT_BY_MAIN_FILE_COPYRIGHT = False
 # Creating a soft test on remotes to see if Edusources team updated their s***
 if MODE != "localhost":
     OPENSEARCH_PRESET_DEFAULT = "products:default"
+    OPENSEARCH_STRICT_MULTILINGUAL_FIELDS = True
