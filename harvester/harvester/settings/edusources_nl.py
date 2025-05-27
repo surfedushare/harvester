4afbcad39885bf23fd00c51951cbe314cfa0ed71
Fako Berkers <email@fakoberkers.nl>
2025-05-27 10:58:40 +0200

    Fixes tests for temporary changes

--- harvester/harvester/settings/edusources_nl.py
+++ harvester/harvester/settings/edusources_nl.py
@@ -10,4 +10,6 @@ SIMPLE_METADATA_FREQUENCY_FIELDS = ["study_vocabulary"]
 
 SET_PRODUCT_COPYRIGHT_BY_MAIN_FILE_COPYRIGHT = False
 
-OPENSEARCH_PRESET_DEFAULT = "products:default"
+# Creating a soft test on remotes to see if Edusources team updated their s***
+if MODE != "localhost":
+    OPENSEARCH_PRESET_DEFAULT = "products:default"
