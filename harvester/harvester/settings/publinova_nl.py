3c180171a3f5284a5d7ea16f154e3f03e8cab13d
Fako Berkers <email@fakoberkers.nl>
2025-06-10 17:23:56 +0200

    Auto succeeds URL checks for Hanze, because their middleware does not support HEAD

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -24,7 +24,7 @@ SHAREKIT_TEST_ORGANIZATIONS = [
 
 SIMPLE_METADATA_FREQUENCY_FIELDS = []
 
-CHECK_URL_AUTO_SUCCEED_SETS = ["saxion:kenniscentra"]
+CHECK_URL_AUTO_SUCCEED_SETS = ["saxion:kenniscentra", "hanze:hanze"]
 DEFAULT_FILE_TITLES_TEMPLATE = "Attachment {ix}"
 
 OPENSEARCH_PRESET_DEFAULT = "products:default"
