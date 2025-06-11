e8ff46d54f044dd30ba10edac87b9370efdb380a
Fako Berkers <email@fakoberkers.nl>
2025-06-11 13:06:35 +0200

    Fixes bug with load-data where download folder didn't match load folder

--- harvester/harvester/management/commands/load_harvester_data.py
+++ harvester/harvester/management/commands/load_harvester_data.py
@@ -104,7 +104,9 @@ class Command(base.LabelCommand):
             logger.info(f"Downloading dump files for: {app_label}")
             ctx = Context(environment)
             source_data = f"s3://{source_environment.aws.harvest_content_bucket}/datasets/harvester/{app_label}"
-            ctx.run(f"aws s3 sync {source_data} {settings.DATAGROWTH_DATA_DIR}", echo=True)
+            ctx.run(f"aws s3 sync {source_data} {settings.DATAGROWTH_DATA_DIR}/{app_label}", echo=True)
+            source_metadata = f"s3://{source_environment.aws.harvest_content_bucket}/datasets/harvester/metadata"
+            ctx.run(f"aws s3 sync {source_metadata} {settings.DATAGROWTH_DATA_DIR}/metadata", echo=True)
 
         logger.info(f"Importing data for: {app_label}")
         for entry in os.scandir(get_dumps_path(storages.Dataset)):
