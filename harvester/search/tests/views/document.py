7495933ca2e114e618524ed8c731b9fb35c20413
Fako Berkers <email@fakoberkers.nl>
2025-06-27 11:46:09 +0200

    Corrects test data generation and adjust asserts (title explain case covered by search-client)

--- harvester/search/tests/views/document.py
+++ harvester/search/tests/views/document.py
@@ -370,20 +370,14 @@ class TestResearchProductExplainSearchView(OpenSearchTestCaseMixin, TestExplainS
             "onderzoek": [
                 "texts.nl.descriptions.text",
                 "texts.nl.contents.text.folded",
-                "texts.nl.titles.text",
                 "texts.nl.descriptions.text.folded",
                 "texts.nl.contents.text.analyzed",
                 "texts.nl.descriptions.text.analyzed",
                 "texts.nl.contents.text",
-                "texts.nl.titles.text.analyzed",
-                "texts.nl.titles.text.folded"
-
             ],
             "over": [
                 "texts.nl.contents.text.folded",
-                "texts.nl.titles.text",
-                "texts.nl.contents.text",
-                "texts.nl.titles.text.folded"
+                "texts.nl.contents.text"
             ]
         })
 
@@ -405,8 +399,7 @@ class TestLearningMaterialsExplainSearchView(OpenSearchTestCaseMixin, TestExplai
         data = response.json()
         self.assert_explain_results(data, {
             "wiskund": [
-                "texts.nl.contents.text.analyzed",
-                "texts.nl.titles.text.analyzed"
+                "texts.nl.contents.text.analyzed"
             ],
             "wiskunde": [
                 "texts.nl.contents.text.folded",
