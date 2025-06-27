21b482b4f608c8ec5adbdf420e5acde3748ded9e
Fako Berkers <email@fakoberkers.nl>
2025-06-27 12:13:28 +0200

    Removes broken print available images command

--- commands/deploy/__init__.py
+++ commands/deploy/__init__.py
@@ -1,2 +1,2 @@
-from .container import prepare_builds, build, push, promote, print_available_images, publish_tika_image
+from .container import prepare_builds, build, push, promote, publish_tika_image
 from .deploy import deploy
