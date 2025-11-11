72a973c45ca221a3c7bfd5b37abb3ee2165068ab
jjmurre <jan.murre@catalyz.nl>
2025-11-11 17:02:41 +0100

    Revert "Filter publication with file"
    This reverts commit 73599a5fb00ffbdd363468ec2fd77e5dfa48e5d8.

--- harvester/products/admin.py
+++ harvester/products/admin.py
@@ -2,18 +2,13 @@ from django.contrib import admin
 
 from core.admin.datatypes import DatasetAdmin, DatasetVersionAdmin, SetAdmin, DocumentAdmin, OverwriteAdmin
 from core.admin.harvest import HarvestStateAdmin
-from core.admin.filters import HasMaterialListFilter
 from products.models import Dataset, DatasetVersion, Set, ProductDocument, HarvestState, Overwrite
-
-
-class ProductDocumentAdmin(DocumentAdmin):
-    list_filter = DocumentAdmin.list_filter + (HasMaterialListFilter,)
 
 
 admin.site.register(Dataset, DatasetAdmin)
 admin.site.register(DatasetVersion, DatasetVersionAdmin)
 admin.site.register(Set, SetAdmin)
-admin.site.register(ProductDocument, ProductDocumentAdmin)
+admin.site.register(ProductDocument, DocumentAdmin)
 admin.site.register(Overwrite, OverwriteAdmin)
 
 admin.site.register(HarvestState, HarvestStateAdmin)
